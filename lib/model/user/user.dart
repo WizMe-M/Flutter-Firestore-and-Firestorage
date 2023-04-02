d49a1e9115c77f3e0d11c6071d3a9d1cbcae43be
WizMe-M <timkin.moxim@mail.ru>
2023-04-02 12:41:03 +0300

    user model

---
+++ lib/model/user/user.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user.freezed.dart';
+
+part 'user.g.dart';
+
+@unfreezed
+class User with _$User {
+  factory User({
+    required String email,
+    required String password,
+  }) = _User;
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+}
